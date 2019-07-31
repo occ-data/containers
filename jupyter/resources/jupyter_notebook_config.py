342cdbd340ca68ae33bdf1c48c6f2f212f291f0a
Edward Malinowski <edwardmalinowski@Edwards-MacBook-Pro.local.cdis>
2019-07-31 18:41:49 -0500

    fix(update-canine): updated sower block in canine

--- jupyter/resources/jupyter_notebook_config.py
+++ jupyter/resources/jupyter_notebook_config.py
@@ -1,4 +1,6 @@
 c.NotebookApp.tornado_settings = {
     'headers': {
+        'Content-Security-Policy':
+        "frame-ancestors 'self' 'https://qa-test4.planx-pla.net/workspace/' 'http://qa-test3.planx-pla.net'"
     }
 }
