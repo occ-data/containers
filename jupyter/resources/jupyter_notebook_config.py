8c8332aeee1083fbbe9366b0526b4af9e6c68117
Edward Malinowski <edwardmalinowski@Edwards-MacBook-Pro.local.cdis>
2019-07-31 19:23:19 -0500

    chore(add-frame-ancestors): Add frame-ancestors

--- jupyter/resources/jupyter_notebook_config.py
+++ jupyter/resources/jupyter_notebook_config.py
@@ -1,6 +1,6 @@
 c.NotebookApp.tornado_settings = {
     'headers': {
         'Content-Security-Policy':
-        "frame-ancestors 'self' 'https://qa-test4.planx-pla.net/workspace/' 'http://qa-test3.planx-pla.net'"
+        "frame-ancestors 'self' 'https://qa-test4.planx-pla.net' 'http://qa-test3.planx-pla.net'"
     }
 }
